4e59ece235c51fedf4414159ba2107fa895dd5a5
unknown <KASUN@.(none)>
2011-09-24 13:29:08 +0530

    display the invoice details

---
+++ invoice.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>invoice</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body bgcolor="#333333"#000000">
+
+
+<div id="head"></div>
+
+<div id="main">
+<div id="invoice">
+    
+    <div id="invoice1">
+        <div id="invoiceheading">Invoice<hr /></div>
+            <div class="invoicedetails">
+            <div class="invoicedin1"><strong>Invoice summary</strong></div>
+            <div class="invoicedin2"></div>
+            </div>
+            <div class="invoicedetails">
+            <div class="invoicedin1"><strong>Date</strong></div>
+            <div class="invoicedin2"></div>
+         </div>
+           <div class="invoicedetails">
+             <div class="invoicedin1"><strong>Invoice number</strong></div>
+            <div class="invoicedin2"></div>
+          </div>
+            <div class="invoicedetails">
+            <div class="invoicedin1"><strong>Due on</strong></div>
+            <div class="invoicedin2"></div>
+        </div>
+            <div class="invoicedetails">
+            <div class="invoicedin1"><strong>Client details</strong></div>
+            <div class="invoicedin2"></div>
+        </div>
+    </div>
+    
+    
+    
+    <div id="invoice2">
+        <div class="invoicedetails">
+            <div class="item"><strong>Item name &amp; description</strong></div>
+            <div class="itemdetails"><strong>Qty.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></div>
+            <div class="itemdetails"><strong>Unit price</strong></div>
+            <div class="itemdetails"><strong>Total</strong></div>
+            
+      </div>
+        <div class="invoicedetails">
+            <div class="item"></div>
+            <div class="itemdetails"><hr /></div>
+            <div class="itemdetails"><hr /></div>
+            <div class="itemdetails"><hr /></div>
+        </div>
+        
+         <div class="invoicedetails">
+            <div class="item"></div>
+            <div class="itemdetails">3</div>
+            <div class="itemdetails">100</div>
+            <div class="itemdetails">300</div>
+        </div>
+        
+        
+        <div class="invoicedetails">
+            <div class="item"></div>
+            <div class="itemdetails">Sub total</div>
+            <div class="itemdetails"></div>
+            <div class="itemdetails"></div>
+        </div>
+        <div class="invoicedetails">
+            <div class="item"></div>
+            <div class="itemdetails">Total due</div>
+            <div class="itemdetails"></div>
+            <div class="itemdetails"></div>
+      </div>
+  </div>
+    
+    
+    
+    <div id="invoice3">
+        <div class="invoicedetails"><strong>Invoice notes</strong></div>
+        <div id="invoicenote">
+          <textarea cols="96" rows="3" class="text"></textarea>
+          
+        </div>
+      <div class="invoicedetails1"><strong>Payments</strong></div>
+        <div class="invoicedetails"> No Payment has been made to this invoice. </div>
+    </div>
+    
+</div>
+
+</div>
+
+<div id="foot"></div>
+
+</body>
+</html>
